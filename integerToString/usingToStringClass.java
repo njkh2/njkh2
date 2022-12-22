af81797a73709a75fde957614a2922b847946eda
njkh2 <110951736+njkh2@users.noreply.github.com>
2022-12-22 19:20:16 +0000

    Add files via upload

---
+++ integerToString/usingToStringClass.java
@@ -0,0 +1,10 @@
+package integerToString;
+
+public class usingToStringClass {
+    public static void main(String[] args) {
+        int num = 1234;
+        Integer obj = new Integer(num);
+        String str = obj.toString();
+        System.out.println(str);
+    }
+}
