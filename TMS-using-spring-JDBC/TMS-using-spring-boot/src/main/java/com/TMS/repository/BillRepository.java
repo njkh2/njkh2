1f895b6d28c0a159c34092ea120d88f7bf6020ab
njkh2 <110951736+njkh2@users.noreply.github.com>
2023-08-06 18:43:59 +0530

    Delete TMS-using-spring-JDBC/TMS-using-spring-boot directory

--- TMS-using-spring-JDBC/TMS-using-spring-boot/src/main/java/com/TMS/repository/BillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.TMS.repository;
-
-import com.TMS.entity.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BillRepository extends JpaRepository<Bill,Integer> {
-}
