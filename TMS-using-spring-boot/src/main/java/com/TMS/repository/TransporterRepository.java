ce8c8df28b586964a8228405dd4ad1a19cd126f6
njkh2 <110951736+njkh2@users.noreply.github.com>
2023-08-14 20:54:33 +0530

    Delete TMS-using-spring-boot directory

--- TMS-using-spring-boot/src/main/java/com/TMS/repository/TransporterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.TMS.repository;
-
-import com.TMS.entity.Transporter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransporterRepository extends JpaRepository<Transporter,Integer> {
-}
